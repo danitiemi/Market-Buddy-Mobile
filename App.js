515c3c3ccea277a56bab31e02fa0dd1056828c20
danitiemi <danitiemi.br@gmail.com>
2018-07-27 14:32:10 -0700

    Install watchman

--- App.js
+++ App.js
@@ -5,8 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>Market Buddy</Text>
       </View>
     );
   }
